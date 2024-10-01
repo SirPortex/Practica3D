c04053bd725b22f0ef8d00c5d39acb0a2ced7761
SirPortex <portex144@gmail.com>
2024-10-01 13:23:54 +0200

    Kinda Finished

--- Assets/Scripts/SaveDataJSON.cs
+++ Assets/Scripts/SaveDataJSON.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using UnityEngine;
+//using static Points;
 
 [System.Serializable]
 struct PlayerData
 {
     public Vector3 position;
-    public int score;
+    public int point;
+    public string date;
 }
 
 
@@ -15,46 +19,53 @@ public class SaveDataJSON : MonoBehaviour
 {
     public string fileName = "Save.json";
 
-
     // Start is called before the first frame update
     void Start()
     {
-        Points point = GetComponent<Points>();
+        
+        fileName= Application.persistentDataPath +'\\'+fileName;
 
-        fileName = Application.persistentDataPath + '\\' + fileName;
-
-        StreamWriter streamWritter = new StreamWriter(fileName);
-        PlayerData playerData = new PlayerData(); //instancio el objeto que vamos a guardar
-        playerData.position = transform.position; //rellenamos la info del objeto
-
-        //playerData.score = Points
-
-        string json = JsonUtility.ToJson(playerData); //"To.Json" -> recibe un objeto serializable y nos genera un string de ese objeto serializable.
-        streamWritter.WriteLine(json);
-
-        streamWritter.Close();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        //Points point = GetComponent<Points>();
         if (File.Exists(fileName)) //Si el archivo existe se guarda
         {
+            //Points point = GetComponent<Points>();
+
             StreamReader streamReader = new StreamReader(fileName);
 
-            string json = streamReader.ReadToEnd();
-            streamReader.Close();
+            Points point = GetComponent<Points>();
 
             try
             {
-                PlayerData playerData = JsonUtility.FromJson<PlayerData>(json); // "FromJson" -> De JSON a objeto. Leemos todo de principio a fin. Te devuelve en formato JSON
+                PlayerData playerData = JsonUtility.FromJson<PlayerData>(streamReader.ReadToEnd()); // "FromJson" -> De JSON a objeto. Leemos todo de principio a fin. Te devuelve en formato JSON
                 transform.position = playerData.position;
+                point.coins = playerData.point;
+
+                //DateTime date = DateTime.Now;
             }
             catch (System.Exception e)
             {
                 //Sacar al topo de Animal Crossing
                 Debug.Log(e.Message);
             }
+            streamReader.Close();
         }
+        
+    }
+
+    // Update is called once per frame
+    private void OnApplicationQuit()
+    {
+        Points point = GetComponent<Points>();
+
+        StreamWriter streamWritter = new StreamWriter(fileName);
+        PlayerData playerData = new PlayerData(); //instancio el objeto que vamos a guardar
+        playerData.position = transform.position; //rellenamos la info del objeto
+        playerData.point = point.coins;
+        playerData.date = DateTime.Now.ToString();
+
+        string json = JsonUtility.ToJson(playerData); //"To.Json" -> recibe un objeto serializable y nos genera un string de ese objeto serializable.
+        streamWritter.WriteLine(json);
+
+        streamWritter.Close();
     }
 }
--- Assets/Scripts/SaveDataTXT.cs
+++ Assets/Scripts/SaveDataTXT.cs
@@ -14,8 +14,6 @@ public class SaveDataTXT : MonoBehaviour
 {
     public string fileName = "SaveFile.txt";
 
-    
-
     // Start is called before the first frame update
     void Start()
     {
@@ -52,6 +50,7 @@ public class SaveDataTXT : MonoBehaviour
     {
         //guardar
         Points point = GetComponent<Points>();
+
         var streamWritter = new StreamWriter(Application.persistentDataPath + '\\' + fileName);
         streamWritter.WriteLine("Archivo de guardado");
         streamWritter.WriteLine(transform.position.x);
@@ -65,120 +64,6 @@ public class SaveDataTXT : MonoBehaviour
 
         streamWritter.WriteLine(DateTime.Now);
 
-
-       
-
         streamWritter.Close(); //IMPORTANTE !!! CERRAR
-    }
-
-    private void OnApplicationFocus()
-    {
-        
-    }
-    //private void Awake()
-    //{
-    //    if (File.Exists(Application.persistentDataPath + '\\' + fileName))
-    //    {
-    //        try
-    //        {
-    //            //Cargar informacion
-    //            StreamReader streamReader = new StreamReader(Application.persistentDataPath + '\\' + fileName);
-    //            streamReader.ReadLine(); //La primera linea no es importante, movemos el cursor del archivo a la siguiente linea
-    //            float x = float.Parse(streamReader.ReadLine()); //Parse -> Pasar de un string a otro tipo. EJ: pasar de string a float.
-    //            float y = float.Parse(streamReader.ReadLine());
-    //            float z = float.Parse(streamReader.ReadLine());
-    //            int c = int.Parse(streamReader.ReadLine());
-
-    //            streamReader.Close();
-
-    //            Points point = GetComponent<Points>(); //recupero el objeto Points
-    //            point.coins = c;
-
-    //            transform.position = new Vector3(x, y, z);
-
-    //        }
-    //        catch (System.Exception e) //como no guardamos la info en ningun server, guardamos en LOCAL.
-    //        {                         // No tenemos control sobre los archivos del usuario. Nos aseguramos de que si algo va mal, este todo controlado.
-    //                                  //sacar al topo de AC
-    //            Debug.Log(e.Message);
-    //        }
-    //    }
-    //}
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            Debug.Log(DateTime.Now);
-        }
-        //if (Input.GetKeyDown(KeyCode.G))
-        //{
-            
-        //    Save();
-        //}
-
-        //else if (Input.GetKeyDown(KeyCode.L))
-        //{
-        //    Load();
-        //}
-
-        //void OnApplicationQuit()
-        //{
-        //    Save();
-        //}
-        
-        //void OnApplicationResume()
-        //{
-        //    Load();
-        //}
-
-
-
-        //void Save()
-        //{
-        //    //guardar
-        //    Points point = GetComponent<Points>();
-        //    var streamWritter = new StreamWriter(Application.persistentDataPath + '\\' + fileName);
-        //    streamWritter.WriteLine("Archivo de guardado");
-        //    streamWritter.WriteLine(transform.position.x);
-        //    streamWritter.WriteLine(transform.position.y);
-        //    streamWritter.WriteLine(transform.position.z);
-        //    streamWritter.WriteLine(point.coins);
-
-        //    streamWritter.Close(); //IMPORTANTE !!! CERRAR
-        //}
-
-        //void Load()
-        //{
-        //    if (File.Exists(Application.persistentDataPath + '\\' + fileName))
-        //    {
-        //        try
-        //        {
-        //            //Cargar informacion
-        //            StreamReader streamReader = new StreamReader(Application.persistentDataPath + '\\' + fileName);
-        //            streamReader.ReadLine(); //La primera linea no es importante, movemos el cursor del archivo a la siguiente linea
-        //            float x = float.Parse(streamReader.ReadLine()); //Parse -> Pasar de un string a otro tipo. EJ: pasar de string a float.
-        //            float y = float.Parse(streamReader.ReadLine());
-        //            float z = float.Parse(streamReader.ReadLine());
-        //            int c = int.Parse(streamReader.ReadLine());
-
-        //            streamReader.Close();
-
-        //            Points point = GetComponent<Points>(); //recupero el objeto Points
-        //            point.coins = c;
-
-        //            transform.position = new Vector3(x, y, z);
-                    
-        //        }
-        //        catch (System.Exception e) //como no guardamos la info en ningun server, guardamos en LOCAL.
-        //        {                         // No tenemos control sobre los archivos del usuario. Nos aseguramos de que si algo va mal, este todo controlado.
-        //            //sacar al topo de AC
-        //            Debug.Log(e.Message);
-        //        }
-        //    }
-        //}
     }
 }
