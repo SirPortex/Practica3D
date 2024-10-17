baa198c513748af127f37519bf7265b299f1c75a
SirPortex <portex144@gmail.com>
2024-10-17 10:52:49 +0200

    complex ia

--- Assets/Scripts/IA/State.cs
+++ Assets/Scripts/IA/State.cs
@@ -8,11 +8,14 @@ using UnityEngine;
 public struct StateParameters
 {
     [Tooltip("Indicates if the action´s check must be true or false")]
-    public bool actionValue;
+    public bool[] actionValues;
     [Tooltip("Action that is gonna be executed")]
-    public Action action;
+    public Action[] actions;
     [Tooltip("If the actions´s check equals acionValue, nextState is pushed")]
     public State nextState;
+    [Tooltip("All actoins must be cheked if they are all true")]
+    public bool and;
+
 }
 
 public abstract class State : ScriptableObject
@@ -21,7 +24,7 @@ public abstract class State : ScriptableObject
 
     public StateParameters[] stateParameters;
 
-    public void DrawnAllActionsGizmos( GameObject owner)
+    public void DrawnAllActionsGizmos(GameObject owner)
     {
         //for (int i = 0; i < stateParameters.Length; i++)
         //{
@@ -30,19 +33,50 @@ public abstract class State : ScriptableObject
 
         foreach (StateParameters parameters in stateParameters)
         {
-            parameters.action.DrawGizmos(owner);
+            foreach(Action action in parameters.actions) // recorre toas las acciones
+            {
+                Gizmos.color = Color.yellow;
+                action.DrawGizmos(owner);
+            }
         }
     }
 
     protected State CheckActions(GameObject owner) //devolvera true si alguna de sus acciones de cumple, o false si es el contrario
     {
-        for (int i = 0; i < stateParameters.Length; i++)
+        for (int i = 0; i < stateParameters.Length; i++) // Recorremos el array de los parametros que tenemos en este for
         {
-            if (stateParameters[i].actionValue == stateParameters[i].action.Check(owner)) //chekea si se cumple 
+            bool allActionsTrue = true;
+            for (int j = 0; j < stateParameters[i].actions.Length; j++) // recorremos el array de las acciones que se tienen que repetir
             {
-                return (stateParameters[i].nextState);
+                if (stateParameters[i].actions[j].Check(owner) == stateParameters[i].actionValues[j]) //si se cumplen todas las condiciones
+                {
+                    if (!stateParameters[i].and)//si solo se tiene que cumplir una
+                    {
+                        //devolvemos directamente el siguiente estado
+                        return stateParameters[i].nextState;
+                    }
+
+                }
+                else if (stateParameters[i].and)
+                {
+                    allActionsTrue = false;
+
+                    //estamos seguros de que esta accion no se ha cumplido
+                    //y el disenador me ha marcado que se tienen que cumplir todas.
+
+                    break; // Se sale del bucle, lo rompe. Por que una accion no se ha cumplido y estamos en "and".
+                }
             }
+
+            //Si llegamos hasta aqui, significa que el disenador ha marcado que todas las acciones
+            //tienen que cumplirse. Tenemos que comprobar si de verdad se han cumplido todas.s
+            if (stateParameters[i].and && allActionsTrue)
+            {
+                return stateParameters[i].nextState; // Si esta puesto el and 
+            }
+
         }
+
         return null; // ninguna accion se cumple por lo que no cambiamos de estado
     }
 
