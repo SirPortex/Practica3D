49965739329a1c4a1d0ec6eec21ecf69b112512a
SirPortex <portex144@gmail.com>
2024-10-21 14:17:45 +0200

    FixedAnimationsStates

--- Assets/Scripts/IA/GuardState.cs
+++ Assets/Scripts/IA/GuardState.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +10,18 @@ public class GuardState : State
 {
     public Vector3 guardPoint;
 
+    public string blendParameter;
+
     public override State Run(GameObject owner)
     {
         State nextState = CheckActions(owner);
 
         NavMeshAgent navMeshAgent = owner.GetComponent<NavMeshAgent>();
+        Animator anim = owner.GetComponent<Animator>();
+
+
         navMeshAgent.SetDestination(guardPoint);
+        anim.SetFloat(blendParameter, navMeshAgent.velocity.magnitude / navMeshAgent.speed);
 
         return nextState;
     }
